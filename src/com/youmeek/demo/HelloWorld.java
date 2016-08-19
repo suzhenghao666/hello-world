9fb574083f832b1d25a8a4e93f5748bd3f446a4f
unknown <>
2016-08-19 17:10:49 +0800

    Initial commit

---
+++ src/com/youmeek/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.youmeek.demo;
+
+/**
+ * Created by 苏政豪 on 2016/8/19 0019.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("\"你好啊\"");
+    }
+}
